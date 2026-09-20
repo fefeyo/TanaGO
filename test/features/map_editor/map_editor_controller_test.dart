e26bc27478ce267e707809b75dc6b8a61873a460
fefe <imanity.fefe@gmail.com>
2026-09-20 14:44:46 +0900

    test: cover map object editing

---
+++ test/features/map_editor/map_editor_controller_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tanago/src/features/map_editor/domain/map_object.dart';
+import 'package:tanago/src/features/map_editor/presentation/map_editor_controller.dart';
+
+void main() {
+  test('adds and edits a shelf', () {
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    final controller = container.read(mapEditorProvider.notifier);
+    controller.add(type: MapObjectType.shelf, x: 1, y: 2);
+
+    final added = container.read(mapEditorProvider).single;
+    expect(added.width, 3);
+    expect(added.height, 1);
+
+    controller.move(id: added.id, x: 4, y: 5);
+    controller.resize(id: added.id, width: 5, height: 2);
+    controller.updateDetails(
+      id: added.id,
+      label: '乳製品',
+      categoryIds: const ['dairy'],
+    );
+
+    final edited = container.read(mapEditorProvider).single;
+    expect(edited.x, 4);
+    expect(edited.y, 5);
+    expect(edited.width, 5);
+    expect(edited.height, 2);
+    expect(edited.label, '乳製品');
+    expect(edited.categoryIds, const ['dairy']);
+  });
+
+  test('clears a shelf label', () {
+    final container = ProviderContainer();
+    addTearDown(container.dispose);
+
+    final controller = container.read(mapEditorProvider.notifier);
+    controller.add(type: MapObjectType.shelf, x: 0, y: 0);
+
+    final id = container.read(mapEditorProvider).single.id;
+    controller.updateDetails(id: id, label: '精肉');
+    controller.updateDetails(id: id, label: '');
+
+    expect(container.read(mapEditorProvider).single.label, isNull);
+  });
+}
