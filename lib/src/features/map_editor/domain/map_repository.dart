796f6ccb7ad860243ed56f549edc5480d18578d0
fefe <imanity.fefe@gmail.com>
2026-09-20 15:52:23 +0900

    feat: add repository contracts

---
+++ lib/src/features/map_editor/domain/map_repository.dart
@@ -0,0 +1,11 @@
+import 'map_object.dart';
+
+abstract interface class MapRepository {
+  Stream<List<MapObject>> watchObjects(String storeId);
+
+  Future<void> saveObject(String storeId, MapObject object);
+
+  Future<void> removeObject(String storeId, String objectId);
+
+  Future<void> clear(String storeId);
+}
