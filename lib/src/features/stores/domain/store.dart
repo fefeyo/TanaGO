d812be7bb29c064f6ef0014f1a7e3ee56afc3e1b
fefe <imanity.fefe@gmail.com>
2026-09-20 14:20:22 +0900

    feat: make shopping data household-aware

--- lib/src/features/stores/domain/store.dart
+++ lib/src/features/stores/domain/store.dart
@@ -1,12 +1,14 @@
 class Store {
   const Store({
     required this.id,
+    required this.householdId,
     required this.name,
     required this.mapWidth,
     required this.mapHeight,
   });
 
   final String id;
+  final String householdId;
   final String name;
   final int mapWidth;
   final int mapHeight;
