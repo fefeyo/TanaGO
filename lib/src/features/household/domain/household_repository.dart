7b7f3182717d80516d0e9ba3fb768633b1306194
fefe <imanity.fefe@gmail.com>
2026-09-20 20:20:16 +0900

    feat: add household onboarding state

---
+++ lib/src/features/household/domain/household_repository.dart
@@ -0,0 +1,20 @@
+import 'household.dart';
+
+abstract interface class HouseholdRepository {
+  Stream<Household?> watchCurrentHousehold(String uid);
+  Stream<List<HouseholdMember>> watchMembers(String householdId);
+
+  Future<Household> createHousehold({
+    required String name,
+    required String ownerUid,
+    required String ownerDisplayName,
+  });
+
+  Future<Household> joinHousehold({
+    required String inviteCode,
+    required String uid,
+    required String displayName,
+  });
+
+  Future<String> getInviteCode(String householdId);
+}
