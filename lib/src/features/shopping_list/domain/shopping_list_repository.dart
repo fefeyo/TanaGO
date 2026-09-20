ac5465ea42ebfbfa5f72493abb6525857038947c
fefe <imanity.fefe@gmail.com>
2026-09-20 15:53:04 +0900

    feat: add repository snapshot reads

--- lib/src/features/shopping_list/domain/shopping_list_repository.dart
+++ lib/src/features/shopping_list/domain/shopping_list_repository.dart
@@ -2,10 +2,8 @@ import 'shopping_item.dart';
 
 abstract interface class ShoppingListRepository {
   Stream<List<ShoppingItem>> watchItems(String householdId);
-
+  Future<List<ShoppingItem>> getItems(String householdId);
   Future<void> addItem(String householdId, ShoppingItem item);
-
   Future<void> updateItem(String householdId, ShoppingItem item);
-
   Future<void> removeItem(String householdId, String itemId);
 }
