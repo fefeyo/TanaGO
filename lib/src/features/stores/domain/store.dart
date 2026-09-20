69ec9e48b233809cd31c632fe575284e602d8348
fefe <imanity.fefe@gmail.com>
2026-09-20 14:10:10 +0900

    chore: bootstrap TanaGO Flutter MVP

---
+++ lib/src/features/stores/domain/store.dart
@@ -0,0 +1,13 @@
+class Store {
+  const Store({
+    required this.id,
+    required this.name,
+    required this.mapWidth,
+    required this.mapHeight,
+  });
+
+  final String id;
+  final String name;
+  final int mapWidth;
+  final int mapHeight;
+}
