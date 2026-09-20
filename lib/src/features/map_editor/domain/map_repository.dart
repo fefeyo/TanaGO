35b49e1ce0964f00d9f3e03f5dea4a95f8c9a1d0
fefe <imanity.fefe@gmail.com>
2026-09-20 15:53:07 +0900

    feat: add repository snapshot reads

--- lib/src/features/map_editor/domain/map_repository.dart
+++ lib/src/features/map_editor/domain/map_repository.dart
@@ -2,10 +2,8 @@ import 'map_object.dart';
 
 abstract interface class MapRepository {
   Stream<List<MapObject>> watchObjects(String storeId);
-
+  Future<List<MapObject>> getObjects(String storeId);
   Future<void> saveObject(String storeId, MapObject object);
-
   Future<void> removeObject(String storeId, String objectId);
-
   Future<void> clear(String storeId);
 }
