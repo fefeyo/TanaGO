8c054d1d85f7caee6e044566649c7b9064065bcf
fefe <imanity.fefe@gmail.com>
2026-09-21 18:18:46 +0900

    feat: connect Firebase authentication

--- lib/src/features/auth/presentation/auth_controller.dart
+++ lib/src/features/auth/presentation/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../data/firebase_auth_repository.dart';
